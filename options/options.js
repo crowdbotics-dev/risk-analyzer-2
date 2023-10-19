a312fa77b7e0bc33bd2259604b20dc7fee4c6d7a
Crowdbotics <engineering@crowdbotics.com>
2023-10-19 17:31:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "risk-analyzer-2",
+  url: "https://risk-analyzer-2.botics.co",
+  api: "https://risk-analyzer-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
